3ae5d139367406d57f7dd1967013ba5b312f2046
Dundersalt <sindre_clausen@hotmail.com>
2022-07-18 22:38:54 +0200

    Please enter the commit message for your changes. Lines starting

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import handGesture from './hand-gesture.png';
 import './App.css';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={handGesture} className="App-logo" alt="logo" />
-        <h1>Bua pizzeria</h1>        
+        <h1>Bua pizzeria</h1>
+        <Link to="/hallaisen">HALLAISEN</Link> | {""}
       </header>
     </div>
   );
